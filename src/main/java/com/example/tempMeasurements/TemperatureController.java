4d9c16c3dcc38663e7d291aa8a8f130ec249b920
kazmierzq <wroblewski.konrad.kk@gmail.com>
2022-07-27 02:45:52 +0200

    Feat seperate calculating and showing data for temp averages, fix data presentation table

--- src/main/java/com/example/tempMeasurements/TemperatureController.java
+++ src/main/java/com/example/tempMeasurements/TemperatureController.java
@@ -27,20 +27,44 @@ public class TemperatureController {
 
     @GetMapping(value = "/history")
     public String results(Model model,
-                          @RequestParam(value = "date", required = false) String date,
-                          @RequestParam(value = "city", required = false) String city) throws ParseException {
-        model.addAttribute("cityNamesSet", service.getAllUniqueCityNames());
-        model.addAttribute("datesSet", service.getAllUniqueDates());
-        try {
-            model.addAttribute("averageCalculatedForDay", service.getAverageByRequestForCity(date, city, 1));
-            model.addAttribute("averageCalculatedFor0622", service.getAverageByRequestForCity(date, city, 2));
-            model.addAttribute("averageCalculatedFor2206", service.getAverageByRequestForCity(date, city, 3));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+                          @RequestParam(value = "date", required = false) String date) {
+        model.addAttribute("getAllUniqueDates", service.getAllUniqueDates());
+        model.addAttribute("getResultsForDate", service.getResultsForDate(date));
+        model.addAttribute("getResultsForDateNational", service.getResultForNational(date));
         model.addAttribute("averageCalculatedForNational", service.getAverageForCountry(date));
         return "history";
     }
+
+    @GetMapping(value = "/calculate")
+    public String calculate(Model model,
+                          @RequestParam(value = "date", required = false) String date) throws ParseException {
+        model.addAttribute("getAllUniqueDates", service.getAllUniqueDates());
+        try {
+            model.addAttribute("averageCalculatedForDay", service.recalculateData(date));
+            model.addAttribute("averageCalculatedForDayNational", service.recalculateDataNational(date));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "calculate";
+    }
+
+//    @GetMapping(value = "/history")
+//    public String results(Model model,
+//                          @RequestParam(value = "date", required = false) String date,
+//                          @RequestParam(value = "city", required = false) String city) throws ParseException {
+//        model.addAttribute("cityNamesSet", service.getAllUniqueCityNames());
+//        model.addAttribute("datesSet", service.getAllUniqueDates());
+//        try {
+//            model.addAttribute("averageCalculatedForDay", service.getAverageByRequestForCity(date, city, 1));
+//            model.addAttribute("averageCalculatedFor0622", service.getAverageByRequestForCity(date, city, 2));
+//            model.addAttribute("averageCalculatedFor2206", service.getAverageByRequestForCity(date, city, 3));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        model.addAttribute("averageCalculatedForNational", service.getAverageForCountry(date));
+//        return "history";
+//    }
 
 }
 
--- src/main/java/com/example/tempMeasurements/temperature/TemperatureService.java
+++ src/main/java/com/example/tempMeasurements/temperature/TemperatureService.java
@@ -1,5 +1,6 @@
 package com.example.tempMeasurements.temperature;
 
+import com.example.tempMeasurements.TemperatureAverageCalculation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.text.ParseException;
@@ -18,10 +19,12 @@ public class TemperatureService {
     private Set<String> uniqueCityNames = new TreeSet<>();
     private Set<String> uniqueDates = new TreeSet<>();
 
-    public ConcurrentHashMap<Integer, Temperature> getAllTemperatures() {
-        return repository.getAllTemperatures();
-    }
+    private List<TemperatureAverageCalculation> listForMap = new ArrayList<>();
 
+    private ConcurrentHashMap<String, List<TemperatureAverageCalculation>> mapOfResults = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, String> mapOfNationalResult = new ConcurrentHashMap<>();
+
+    // Add new record of a temperature (together with updates of the unique city names and dates for UI)
     public String addTemperature(Temperature temperature) {
         repository.addTemperature(temperature);
         uniqueCityNames.add(temperature.getCity());
@@ -29,14 +32,15 @@ public class TemperatureService {
         return "Temperature " + temperature.getTemperature() + " has been added for city " + temperature.getCity() + "!";
     }
 
+    // (UI) Acquire all unique city names from the database
     public Set<String> getAllUniqueCityNames() {
         return uniqueCityNames;
     }
 
+    // (UI) Acquire all unique dates from the database
     public Set<String> getAllUniqueDates() {
         return uniqueDates;
     }
-
 
     // Calculations of the average temperature for the whole country of the selected day
         public String getAverageForCountry(String date) {
@@ -104,7 +108,7 @@ public class TemperatureService {
 
         for (Temperature temperatures : map.values()) {
 
-            if (temperatures.getCity().equals(city) && temperatures.getDate().equals(date)) {
+            if (temperatures.getCity().equals(city) && temperatures.getDate().substring(0,10).equals(date)) {
 
                 // For an average of whole `day`
                 if (request == 1) {
@@ -150,6 +154,59 @@ public class TemperatureService {
         }
 
         return resultToString;
+    }
+
+    // Recalculate report for selected date (averages daily, 06:00-22:00, 22:00-06:00 per each city)
+    public String recalculateData(String date) throws ParseException {
+        if (date == null || date.length() != 10) {
+            return "Select proper date";        }
+
+        List<TemperatureAverageCalculation> temporaryList = new ArrayList<>();
+
+        for (String city : uniqueCityNames) {
+            String averageD = getAverageByRequestForCity(date, city, 1);
+            String average0622 = getAverageByRequestForCity(date, city, 2);
+            String average2206 = getAverageByRequestForCity(date, city, 3);
+
+            temporaryList.add(new TemperatureAverageCalculation(city, averageD, average0622, average2206));
+        }
+
+        mapOfResults.put(date, temporaryList);
+
+        return "Calculations for date " + date + " finished";
+    }
+
+    // Recalculate report for selected date (for the whole country)
+    public String recalculateDataNational(String date) {
+        if (date == null || date.length() != 10) {
+            return "Select proper date";        }
+
+        mapOfNationalResult.put(date, getAverageForCountry(date));
+
+        return "Calculation (national) for date " + date + " finished";
+    }
+
+    // Get data from actually created report (averages daily, 06:00-22:00, 22:00-06:00 per each city) for selected day
+    public List<TemperatureAverageCalculation> getResultsForDate(String date) {
+        if (date == null || date.length() != 10) {
+            return listForMap;
+        }
+
+        return (mapOfResults.isEmpty() ? listForMap : mapOfResults.get(date));
+    }
+
+    // Get data from actually created report (for the whole country) for selected day
+    public String getResultForNational(String date) {
+        if (date == null || date.length() != 10) {
+            return "N/A";
+        }
+
+        return (mapOfNationalResult.isEmpty() ? "Recalculation required" : mapOfNationalResult.get(date));
+    }
+
+    // Get all temperatures (as map, for iteration)
+    public ConcurrentHashMap<Integer, Temperature> getAllTemperatures() {
+        return repository.getAllTemperatures();
     }
 }
 
